dee24b542412ebfe9d197746c6da2791bbe862e9
Eneko Izaguirre <eneko.izaguirre@outlook.com>
2021-12-14 01:25:25 +0000

    Still learning jQuery

--- assignment/update/Interface.js
+++ assignment/update/Interface.js
@@ -1,17 +1,17 @@
 $.ajax({
-    url: '../XMLStore.xml',
-    dataType: 'xml',
+    url: 'http://localhost:8080/atwd1/assignment/update/?cur=EUR&action=del',
+    dataType: 'text',
     success: function(data){
-        $('input[type=radio][name=action]').change(function() {
-            if (this.value == 'PUT') {
-                $(data).find('store currencies currency').each(function(){
-                    console.log($(this).attr('code'));
-                })
-            }
-            else if (this.value == 'DEL') {
-                alert(this.value);
-            }
-        })
+    $('input[type=radio][name=action]').change(function() {
+        if (this.value == 'PUT') {
+                // console.log(data));
+                var xmlDoc = jQuery.parseXML(data);
+                $("#response").val(xmlDoc);
+        }
+        else if (this.value == 'DEL') {
+            alert(this.value);
+        }
+    })
     },
     error: function(){
         console.log('ERROR');
