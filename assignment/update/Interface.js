4e8adea9e6f34eb14094efc298c13ddf55ef8899
Eneko Izaguirre Martin <enekoiza@Enekos-MacBook-Pro.local>
2021-12-14 15:35:30 +0000

    Added format to xml output inside the TextArea

--- assignment/update/Interface.js
+++ assignment/update/Interface.js
@@ -1,12 +1,24 @@
 $.ajax({
     url: 'http://localhost:8080/atwd1/assignment/update/?cur=EUR&action=del',
     dataType: 'text',
+    contentType: 'text/plain',
     success: function(data){
     $('input[type=radio][name=action]').change(function() {
         if (this.value == 'PUT') {
-                // console.log(data));
-                var xmlDoc = jQuery.parseXML(data);
-                $("#response").val(xmlDoc);
+                // cm.setValue("");
+                // cm.clearHistory();
+                $("#response").val(data);
+                document.getElementById("response").value = vkbeautify.xml(document.getElementById("response").value);
+                CodeMirror.fromTextArea(document.getElementById("response"), {
+                  mode: 'application/xml',
+                //    theme: 'eclipse',
+                  lineNumbers: true,
+                  lineWrapping: true,
+                  readOnly: true,
+                  cursorBlinkRate: -1
+                });
+                // alert(data);
+
         }
         else if (this.value == 'DEL') {
             alert(this.value);
